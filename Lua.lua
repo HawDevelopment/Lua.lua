9daf9322ee49f738f4bfbd01f0d15775734db55b
HawDevelopment <70876593+HawDevelopment@users.noreply.github.com>
2021-10-10 18:35:09 +0200

    Re-organized Lua.lua and service.lua

--- Lua.lua
+++ Lua.lua
@@ -4,4 +4,101 @@
     07/10/2021
 --]]
 
-require("src.service")(arg)
+local Service = require("src.service")
+
+local Lexer = require("src.Generator.Lexer.Lexer")
+local LexerDebug = require("src.Generator.Lexer.LexerDebug")
+local Parser = require("src.Generator.Parser.Parser")
+local ParserDebug = require("src.Generator.Parser.ParserDebug")
+
+local USAGE = [[
+    USAGE: Lua.lua [SUBCOMMANDS] [FILES]
+    
+    SUBCOMMANDS:
+        run     Runs a file
+        sim     Takes input and interprets it immediately
+]]
+
+local unpack = unpack or table.unpack
+
+local function PrintTokens(tokens)
+    for _, tok in pairs(tokens) do
+        print(tok:rep())
+    end
+end
+
+local function ValueInTable(tab, value)
+    for _, val in pairs(tab) do
+        if val == value then
+            return true
+        end
+    end
+    return false
+end
+
+function RunCommand()
+    if #arg == 0 then
+        print(USAGE)
+        return
+    end
+    
+    -- Parse the command
+    local opt, args = {}, {}
+    for _, value in pairs(arg) do
+        if string.match(value, "^%-%-") ~= nil then
+            opt[#opt+1] = value:sub(3)
+        else
+            args[#args+1] = value
+        end
+    end
+    
+    DEBUG = ValueInTable(opt, "debug")
+    PRINT_ARG = ValueInTable(opt, "print")
+    local lexer = DEBUG and LexerDebug or Lexer
+    local parser = DEBUG and ParserDebug or Parser
+    
+    if args[1] == "run" then
+        local file = io.open(args[2], "r")
+        if not file then
+            return print("File not found")
+        end
+        
+        local source = file:read("*a")
+        file:close()
+        local lexed, parsed = Service(source, lexer, parser, nil, DEBUG)
+        if PRINT_ARG then
+            if type(lexed) == "table" then
+                print("Lexed tokens:")
+                PrintTokens(lexed)
+            end
+            if type(parsed) == "table" then
+                print("Parsed tokens:")
+                PrintTokens(parsed)
+            end
+        end
+        
+    elseif args[1] == "sim" then
+        
+        while true do
+            io.write("> ")
+            local inp = io.read("*l")
+            if inp == "exit" or not inp then
+                return
+            end
+            
+            local lexed, parsed = Service(inp, lexer, parser, nil, DEBUG)
+            if PRINT_ARG then
+                if type(lexed) == "table" then
+                    print("Lexed tokens:")
+                    PrintTokens(lexed)
+                end
+                if type(parsed) == "table" then
+                    print("Parsed tokens:")
+                    PrintTokens(parsed)
+                end
+            end
+        end
+    end
+end
+
+RunCommand()
--- src/service.lua
+++ src/service.lua
@@ -4,38 +4,10 @@
     28/09/2021
 --]]
 
-local PRINT_ARG = false
-local DEBUG = false
-
-local USAGE = [[
-    USAGE: Lua.lua [SUBCOMMANDS] [FILES]
-    
-    SUBCOMMANDS:
-        run     Runs a file
-        sim     Takes input and interprets it immediately
-]]
-
 local Lexer = require("src.Generator.Lexer.Lexer")
-local LexerDebug = require("src.Generator.Lexer.LexerDebug")
 local Parser = require("src.Generator.Parser.Parser")
-local ParserDebug = require("src.Generator.Parser.ParserDebug")
 
 local unpack = unpack or table.unpack
-
-local function PrintTokens(tokens)
-    for _, tok in pairs(tokens) do
-        print(tok:rep())
-    end
-end
-
-local function ValueInTable(tab, value)
-    for _, val in pairs(tab) do
-        if val == value then
-            return true
-        end
-    end
-    return false
-end
 
 local function TakeTime(func, ...)
     local start = os.clock()
@@ -43,70 +15,53 @@ local function TakeTime(func, ...)
     return os.clock() - start, unpack(arg)
 end
 
-local function RunFile(source, lexer, parser, interpreter)
+local function PrintFile(source, lexer, parser, interpreter)
     local starttime = os.clock()
     print("\nLexing:")
     local lextime, tokens = TakeTime(function(...)
         return lexer(...)
     end, source)
-    if PRINT_ARG then
-        PrintTokens(tokens)
-    end
     print("Lexing took: ", lextime .. "s")
     print("\nParsing:")
     local parsetime, parsed = TakeTime(function(...)
         return parser(...)
     end, tokens)
-    if PRINT_ARG then
-        PrintTokens(parsed)
-    end
     print("Parsing took: ", parsetime .. "s")
     print("Total real time: ", (os.clock() - starttime) .. "s")
     print("Total time: ", (parsetime + lextime) .. "s")
-    return parsed
+    return tokens, parsed
 end
 
-return function(arg)
-    if #arg == 0 then
-        print(USAGE)
-        return
+function RunFile(source, lexer, parser)
+    local succes, tokens = pcall(function()
+        return lexer(source)
+    end)
+    if not succes then
+        print("Lexing failed: ", tokens)
+    end
+    local succes, parsed = pcall(function()
+        return parser(tokens)
+    end)
+    if not succes then
+        print("Parsing failed: ", parsed)
+    end
+    return tokens, parsed
+end
+
+return function(source, lexer, parser, interpreter, shouldprint)
+    source = source or ""
+    lexer = lexer or Lexer
+    parser = parser or Parser
+    if shouldprint == nil then
+        shouldprint = true
     end
     
-    -- Parse the command
-    local opt, args = {}, {}
-    for _, value in pairs(arg) do
-        if string.match(value, "^%-%-") ~= nil then
-            opt[#opt+1] = value:sub(3)
-        else
-            args[#args+1] = value
-        end
+    local lexed, parsed
+    if shouldprint then
+        lexed, parsed = PrintFile(source, lexer, parser, interpreter)
+    else
+        lexed, parsed = RunFile(source, lexer, parser)
     end
     
-    DEBUG = ValueInTable(opt, "debug")
-    PRINT_ARG = ValueInTable(opt, "print")
-    local lexer = DEBUG and LexerDebug or Lexer
-    local parser = DEBUG and ParserDebug or Parser
-    
-    if args[1] == "run" then
-        local file = io.open(args[2], "r")
-        if not file then
-            return print("File not found")
-        end
-        
-        local source = file:read("*a")
-        file:close()
-        local parsed = RunFile(source, lexer, parser)
-        
-    elseif args[1] == "sim" then
-        
-        while true do
-            io.write("> ")
-            local inp = io.read("*l")
-            if inp == "exit" or not inp then
-                return
-            end
-            
-            local parsed = RunFile(inp, lexer, parser)
-        end
-    end
+    return lexed, parsed
 end
